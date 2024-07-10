f70a4facce55f48970b97eaeb5feecfe6b4fb306
BenjaAlegre <capacitaciones06.02@adviters.com>
2024-07-10 15:29:57 -0300

    tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,46 @@
+import React from "react";
 import "./App.css";
 import { Footer } from "./components/Footer";
 import Header from "./components/Header";
-import { useCount } from "./hooks/useCount";
+import Table from "./components/table/Table";
 
 const App = () => {
-  const { state, add, desc, reset } = useCount();
 
   const users = [
-    { id: 1, nombre: "Juan" },
-    { id: 2, nombre: "Lautaro" },
-    { id: 3, nombre: "David" },
-    { id: 4, nombre: "Gonzalo" },
-    { id: 5, nombre: "Pablo" },
+    {
+      id: 1,
+      apellido: "Jon",
+      nombre: "Hernandez",
+      edad: 25,
+      examenes: [
+        { tema: "Matematicas", puntaje: 95 },
+        { tema: "Fisica", puntaje: 88 },
+      ],
+      promedio: 1
+    },
+    {
+      id: 2,
+      apellido: "Smith",
+      nombre: "Jane",
+      edad: 30,
+      examenes: [
+        { tema: "Matematicas", puntaje: 85 },
+        { tema: "Fisica", puntaje: 92 },
+      ],
+      promedio: 1
+    },
   ];
 
   return (
     <div
-      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+    >
       <Header />
       <div style={{ flexGrow: 1 }}>
-        {/* <p>Contador {state}</p>
         <div>
-          <button onClick={add}>Aumentar contador</button>
-          <button onClick={desc}>Disminuir contador</button>
-          <button onClick={reset}>Resetear contador</button>
-        </div> */}
-
-        <table>
-          <thead>
-            <tr>
-              {Object.keys(users[0]).map((key) => (
-                <th>{key}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr>
-                <td>{user.id}</td>
-                <td>{user.nombre}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+          <h1>Tabla</h1>
+          <Table users={users} />
+        </div>
       </div>
       <Footer number="12345" />
     </div>
---
+++ src/components/table/Table.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./Table.module.css"
+
+const Table = ({ users }) => {
+  return (
+    <table>
+      <thead>
+        
+        <tr>
+              {Object.keys(users[0]).map((key) => (
+                <th>{key}</th>
+              ))}
+            </tr>
+      </thead>
+      <tbody>
+        {users.map(user => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.apellido}</td>
+            <td>{user.nombre}</td>
+            <td>{user.edad}</td>
+            <td>{user.promedio}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
+/*<table>
+                <thead>
+                  <tr>
+                    <th>Tema</th>
+                    <th>Puntaje</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {user.examenes.map((examen, index) => (
+                    <tr key={index}>
+                      <td>{examen.tema}</td>
+                      <td>{examen.puntaje}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>*/
